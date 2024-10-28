dec626d1ffcb9ac75753878c9884aff55bcc7edf
Nicolas Champion <champion.nicolas@gmail.com>
2024-10-28 07:12:00 +0100

    Update index.tsx

--- typescript/index.tsx
+++ typescript/index.tsx
@@ -2,21 +2,12 @@ import '../resources/tailwind.css'
 import { render } from 'solid-js/web'
 
 function App() {
-
-  fetch("/data",{
-    method: "GET",
-    headers: {"x-sweetheart-action":"fetch.init"} })
-  .then(response => response.json())
-  .then(data => { console.log(data) })
-
   return(
     <div class="my-28">
 
-      <h1 class="text-4xl lg:text-6xl text-pink-500 text-center italic">
-        Welcome to Sweetheart </h1>
+      <h1 class="text-4xl lg:text-6xl text-blue-500 text-center italic">
+        Welcome! </h1>
 
-      <p class="text-center mt-4">
-        get at {window.location.pathname} </p>
     </div>
   )
 }
